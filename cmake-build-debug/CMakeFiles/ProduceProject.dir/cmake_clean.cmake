bfbed38981c7a4913279b6dd104f7776c4cdbcc1
JonathanHo16 <jonathanbho@outlook.com>
2018-11-21 14:39:34 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProduceProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProduceProject.dir/main.cpp.o"
+  "ProduceProject.pdb"
+  "ProduceProject.exe"
+  "libProduceProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProduceProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
