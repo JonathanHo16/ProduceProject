fc71d162dde9f2dcf1b13d99632841ecfcb41682
JonathanHo16 <jonathanbho@outlook.com>
2018-11-26 14:15:12 -0500

    enhanced produce base class

--- FallProduce.hpp
+++ FallProduce.hpp
@@ -6,19 +6,14 @@
 #define PRODUCEPROJECT_WINTERPRODUCE_HPP
 
 #include "Produce.hpp"
+#include <vector>
 
 
 class FallProduce : public Produce
 {
 public:
     string type;
-    string const fruit_list[38] = { "Apples", "Bananas", "Beets", "Bell Peppers", "Broccoli", "Brussels Sprouts", "Cabbage",
-                                    "Carrots", "Cauliflower", "Celery", "Collared Greens", "Cranberries", "Garlic", "Ginger",
-                                    "Grapes", "Green Beans", "Kale", "Kiwifruit", "Lemons", "Lettuce", "Limes", "Mangoes",
-                                    "Mushrooms", "Onions", "Parsnips", "Pears", "Peas", "Pineapples", "Potatoes", "Pumpkins",
-                                    "Radishes", "Raspberries", "Rutabagas", "Spinach", "Sweet Potatoes & Yams", "Swiss Chard",
-                                    "Turnips", "Winter Squash"};
-
+    vector<string>fruit_list;
     FallProduce() : type("Fall"), Produce("n/a", 0, 1, 1, 2000)
     {
 
